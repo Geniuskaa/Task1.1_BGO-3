b3174624c90a97a46527c5c4e814382634706fb7
Emil Zinnatullin <eminizinar@gmail.com>
2020-12-16 10:26:04 +0300

    New branch and second task was removed

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 func main() {
-	// First task
 	purchase := 3333_33
 	percent := 1
 	limit := 100
@@ -11,11 +10,4 @@ func main() {
 		bonus = limit
 	}
 	println(bonus)
-	//////////////////////////////////////////
-
-	// Second task
-	var balance int64 = 15_000_000_00 // 15 миллионов в копейках
-	var transfer int64 = 10_000_000_00 // 10 миллионов в копейках
-	total := (balance + transfer) / 100 // Вывод в рублях, без копеек
-	println(total) //
 }
